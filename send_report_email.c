46944ae3690591b0b11abe05050b2b065780a245
Hashaam-Hasan <148707702+Hashaam-Hasan@users.noreply.github.com>
2024-01-13 22:16:24 +0500

    Update send_report_email.c

---
+++ send_report_email.c
@@ -0,0 +1,59 @@
+#include "send_email.h"
+#include <stdio.h>
+#include <curl/curl.h>
+
+size_t read_callback(void *ptr, size_t size, size_t nmemb, FILE *stream) {
+    size_t retcode = fread(ptr, size, nmemb, stream);
+    return retcode;
+}
+
+int send_email_with_attachment(const char *to, const char *file_path) {
+    CURL *curl;
+    CURLcode res = CURLE_OK;
+
+    FILE *file = fopen(file_path, "rb");
+    if (!file) {
+        fprintf(stderr, "Error opening file: %s\n", file_path);
+        return 1;
+    }
+
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl = curl_easy_init();
+
+    if(curl) {
+        struct curl_slist *recipients = NULL;
+
+        // Specify email server details
+        // For Gmail
+       // Specify email server details
+curl_easy_setopt(curl, CURLOPT_URL, "smtps://smtp.gmail.com");
+curl_easy_setopt(curl, CURLOPT_USERPWD, "smhashaam@gmail.com:brzd ovty gltu hrxm");
+curl_easy_setopt(curl, CURLOPT_USE_SSL, CURLUSESSL_ALL);
+
+
+        // Add email recipients
+        if (to)
+            recipients = curl_slist_append(recipients, to);
+    
+        curl_easy_setopt(curl, CURLOPT_MAIL_RCPT, recipients);
+
+        // Set the read callback function to read the file content
+        curl_easy_setopt(curl, CURLOPT_READFUNCTION, read_callback);
+        curl_easy_setopt(curl, CURLOPT_READDATA, file);
+        curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
+
+        // Perform the email sending
+        res = curl_easy_perform(curl);
+        if(res != CURLE_OK)
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+
+        // Clean up
+        curl_slist_free_all(recipients);
+        curl_easy_cleanup(curl);
+    }
+
+    fclose(file);// Close the file
+
+    curl_global_cleanup();
+    return (int)res;
+}
